8e99584af01f52fcf6fca638907205d92b65b2ce
Lama Alrweta <lamaalruwaita18@gmail.com>
2021-04-24 23:12:39 +0300

    Update Youtub.py

--- Youtub.py
+++ Youtub.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import youtube_dl
+from pytube import YouTube
 
 
 st.title(" YouTube Downloader")
@@ -10,14 +11,15 @@ options = {
 	    }
 submit = st.button("download")
 def download(link):
+	yt = YouTube(link)
 	try:
 		if submit:
-			with youtube_dl.YoutubeDL(options) as ydl:
-				ydl.download([link])
+			# with youtube_dl.YoutubeDL(options) as ydl:
+			# 	ydl.download([link])
+			out_file=yt.stream.filter(progressive=True,file_extension='mp4').order_by('resolution').desc().first().download('sample')
 			st.success("successfully downloaded")
 
-	except youtube_dl.utils.DownloadError:
+	except YouTube.utils.DownloadError:
 		raise st.error('this URL is invalid')
 if __name__ == '__main__':
 	download(link)
-
