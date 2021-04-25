90cabeb95813385c2176a73d480362e2ba70e45a
Lama Alrweta <lamaalruwaita18@gmail.com>
2021-04-25 05:46:56 +0300

    Update Youtub.py

--- Youtub.py
+++ Youtub.py
@@ -1,25 +1,74 @@
 import streamlit as st
-import youtube_dl
 from pytube import YouTube
+import os
 
+if os.name == 'nt':
+    import ctypes
+    from ctypes import windll, wintypes
+    from uuid import UUID
+
+
+    # ctypes GUID copied from MSDN sample code
+    class GUID(ctypes.Structure):
+        _fields_ = [
+            ("Data1", wintypes.DWORD),
+            ("Data2", wintypes.WORD),
+            ("Data3", wintypes.WORD),
+            ("Data4", wintypes.BYTE * 8)
+        ]
+
+        def __init__(self, uuidstr):
+            uuid = UUID(uuidstr)
+            ctypes.Structure.__init__(self)
+            self.Data1, self.Data2, self.Data3, \
+            self.Data4[0], self.Data4[1], rest = uuid.fields
+            for i in range(2, 8):
+                self.Data4[i] = rest >> (8 - i - 1) * 8 & 0xff
+
+
+    SHGetKnownFolderPath = windll.shell32.SHGetKnownFolderPath
+    SHGetKnownFolderPath.argtypes = [
+        ctypes.POINTER(GUID), wintypes.DWORD,
+        wintypes.HANDLE, ctypes.POINTER(ctypes.c_wchar_p)
+    ]
+
+
+    def _get_known_folder_path(uuidstr):
+        pathptr = ctypes.c_wchar_p()
+        guid = GUID(uuidstr)
+        if SHGetKnownFolderPath(ctypes.byref(guid), 0, 0, ctypes.byref(pathptr)):
+            raise ctypes.WinError()
+        return pathptr.value
+
+
+    FOLDERID_Download = '{374DE290-123F-4565-9164-39C4925E467B}'
+
+
+    def get_download_folder():
+        return _get_known_folder_path(FOLDERID_Download)
+else:
+    def get_download_folder():
+        home = os.path.expanduser("~")
+        return os.path.join(home, "Downloads")
 
 st.title(" YouTube Downloader")
-#Enter the URL
+# Enter the URL
 link = st.text_input("Enter the link here")
 options = {
-			"format": "bestvideo+bestaudio"
-	    }
+    "format": "bestvideo+bestaudio"
+}
 submit = st.button("download")
-def download(link):
-	yt = YouTube(link)
-	try:
-		if submit:
-			# with youtube_dl.YoutubeDL(options) as ydl:
-			# 	ydl.download([link])
-			out_file=yt.stream.filter(progressive=True,file_extension='mp4').order_by('resolution').desc().first().download('sample')
-			st.success("successfully downloaded")
 
-	except YouTube.utils.DownloadError:
-		raise st.error('this URL is invalid')
+
+def download(link):
+
+    if submit:
+        yt = YouTube(link)
+        yt = yt.streams.filter(progressive=True, file_extension='mp4').order_by(
+            'resolution').desc().first().download(
+            get_download_folder())
+        st.success("successfully downloaded in 'Downloads Folder' ")
+
+
 if __name__ == '__main__':
-	download(link)
+    download(link)
