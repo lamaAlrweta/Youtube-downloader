72b9b93ffd0906692baf7c524c9770fbd56e2fe8
Lama Alrweta <lamaalruwaita18@gmail.com>
2021-04-27 02:45:09 +0300

    Update Youtub.py

--- Youtub.py
+++ Youtub.py
@@ -1,19 +1,21 @@
 import streamlit as st
 from pytube import YouTube
 import os
-
-def get_download_path():
-    """Returns the default downloads path for linux or windows"""
-    if os.name == 'nt':
-        import winreg
-        sub_key = r'SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders'
-        downloads_guid = '{374DE290-123F-4565-9164-39C4925E467B}'
-        with winreg.OpenKey(winreg.HKEY_CURRENT_USER, sub_key) as key:
-            location = winreg.QueryValueEx(key, downloads_guid)[0]
-        return location
-    else:
-        return os.path.join(os.path.expanduser('~'), 'downloads')
-
+from pathlib import Path
+path_to_download_folder = str(os.path.join(Path.home(), "Downloads"))
+#
+# def get_download_path():
+#     """Returns the default downloads path for linux or windows"""
+#     if os.name == 'nt':
+#         import winreg
+#         sub_key = r'SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders'
+#         downloads_guid = '{374DE290-123F-4565-9164-39C4925E467B}'
+#         with winreg.OpenKey(winreg.HKEY_CURRENT_USER, sub_key) as key:
+#             location = winreg.QueryValueEx(key, downloads_guid)[0]
+#         return location
+#     else:
+#         return os.path.join(os.path.expanduser('~'), 'downloads')
+#
 st.title(" YouTube Downloader")
 # Enter the URL
 link = st.text_input("Enter the link here")
@@ -28,8 +30,7 @@ def download(link):
     if submit:
         yt = YouTube(link)
         yt = yt.streams.filter(progressive=True, file_extension='mp4').order_by(
-            'resolution').desc().first().download(
-            get_download_path())
+            'resolution').desc().first().download(path_to_download_folder)
         st.success("successfully downloaded in 'Downloads Folder' ")
 
 
