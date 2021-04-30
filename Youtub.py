958a8ca2e1c627ac4fbb24c4a326ec46969098d1
Lama Alrweta <lamaalruwaita18@gmail.com>
2021-04-30 21:57:03 +0300

    Update Youtub.py

--- Youtub.py
+++ Youtub.py
@@ -1,21 +1,10 @@
 import streamlit as st
 from pytube import YouTube
 import os
-from pathlib import Path
+from pathlib2 import Path
+
 path_to_download_folder = str(os.path.join(Path.home(), "Downloads"))
-#
-# def get_download_path():
-#     """Returns the default downloads path for linux or windows"""
-#     if os.name == 'nt':
-#         import winreg
-#         sub_key = r'SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders'
-#         downloads_guid = '{374DE290-123F-4565-9164-39C4925E467B}'
-#         with winreg.OpenKey(winreg.HKEY_CURRENT_USER, sub_key) as key:
-#             location = winreg.QueryValueEx(key, downloads_guid)[0]
-#         return location
-#     else:
-#         return os.path.join(os.path.expanduser('~'), 'downloads')
-#
+
 st.title(" YouTube Downloader")
 # Enter the URL
 link = st.text_input("Enter the link here")
@@ -26,13 +15,16 @@ submit = st.button("download")
 
 
 def download(link):
-
     if submit:
-        yt = YouTube(link)
-        yt = yt.streams.filter(progressive=True, file_extension='mp4').order_by(
-            'resolution').desc().first().download(path_to_download_folder)
-        st.success("successfully downloaded in 'Downloads Folder' ")
+        # if link == YouTube:
+            yt = YouTube(link)
+            yt = yt.streams.filter(progressive=True, file_extension='mp4').order_by(
+                'resolution').desc().first().download(path_to_download_folder)
+            st.success("successfully downloaded in Downloads Folder ")
+        # else:
+        #     st.error("Enter a valid YouTube link")
 
 
 if __name__ == '__main__':
     download(link)
+
